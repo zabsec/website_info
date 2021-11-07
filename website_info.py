7792286905e18ae339af7310b84cf8c721f53993
Zabsec <71888760+zabsec@users.noreply.github.com>
2021-11-07 17:32:07 -0500

    Update website_info.py

--- website_info.py
+++ website_info.py
@@ -41,132 +41,28 @@ for x in range(2, ):
 global r
 
 
-def status():  # For each status code, I will add the description and it will show up whenever status() is called upon
+def status():  # For each status code, I will add scthe description and it will show up whenever status() is called upon
     global r
-    if r.status_code == 100:
-        print("Status Code: {}({})".format(r.status_code, "Continue"))
-    elif r.status_code == 101:
-        print("Status Code: {}({})".format(r.status_code, "Switching Protocol"))
-    elif r.status_code == 102:
-        print("Status Code: {}({})".format(r.status_code, "Processing"))
-    elif r.status_code == 103:
-        print("Status Code: {}({})".format(r.status_code, "Early Hints"))
-    elif r.status_code == 200:
-        print("Status Code: {}({})".format(r.status_code, "OK"))
-    elif r.status_code == 201:
-        print("Status Code: {}({})".format(r.status_code, "Created"))
-    elif r.status_code == 202:
-        print("Status Code: {}({})".format(r.status_code, "Non-Authoritative Information"))
-    elif r.status_code == 203:
-        print("Status Code: {}({})".format(r.status_code, "Switching Protocol"))
-    elif r.status_code == 204:
-        print("Status Code: {}({})".format(r.status_code, "No Content"))
-    elif r.status_code == 205:
-        print("Status Code: {}({})".format(r.status_code, "Reset Content"))
-    elif r.status_code == 206:
-        print("Status Code: {}({})".format(r.status_code, "Partial Content"))
-    elif r.status_code == 207:
-        print("Status Code: {}({})".format(r.status_code, "Multi-Status"))
-    elif r.status_code == 208:
-        print("Status Code: {}({})".format(r.status_code, "Already Reported"))
-    elif r.status_code == 226:
-        print("Status Code: {}({})".format(r.status_code, "IM Used"))
-    elif r.status_code == 300:
-        print("Status Code: {}({})".format(r.status_code, "Multiple Choice"))
-    elif r.status_code == 301:
-        print("Status Code: {}({})".format(r.status_code, "Moved Permanently"))
-    elif r.status_code == 302:
-        print("Status Code: {}({})".format(r.status_code, "Found"))
-    elif r.status_code == 303:
-        print("Status Code: {}({})".format(r.status_code, "See Other"))
-    elif r.status_code == 304:
-        print("Status Code: {}({})".format(r.status_code, "Not Modified"))
-    elif r.status_code == 305:
-        print("Status Code: {}({})".format(r.status_code, "Use Proxy"))
-    elif r.status_code == 307:
-        print("Status Code: {}({})".format(r.status_code, "Temporary Redirect"))
-    elif r.status_code == 308:
-        print("Status Code: {}({})".format(r.status_code, "Permanent Redirect"))
-    elif r.status_code == 400:
-        print("Status Code: {}({})".format(r.status_code, "Bad Request"))
-    elif r.status_code == 401:
-        print("Status Code: {}({})".format(r.status_code, "Unauthorized"))
-    elif r.status_code == 402:
-        print("Status Code: {}({})".format(r.status_code, "Payment Required"))
-    elif r.status_code == 403:
-        print("Status Code: {}({})".format(r.status_code, "Forbidden"))
-    elif r.status_code == 404:
-        print("Status Code: {}({})".format(r.status_code, "Not Found"))
-    elif r.status_code == 405:
-        print("Status Code: {}({})".format(r.status_code, "Method Not Allowed"))
-    elif r.status_code == 406:
-        print("Status Code: {}({})".format(r.status_code, "Not Acceptable"))
-    elif r.status_code == 407:
-        print("Status Code: {}({})".format(r.status_code, "Proxy Authentication Required"))
-    elif r.status_code == 408:
-        print("Status Code: {}({})".format(r.status_code, "Request Timeout"))
-    elif r.status_code == 409:
-        print("Status Code: {}({})".format(r.status_code, "Conflict"))
-    elif r.status_code == 410:
-        print("Status Code: {}({})".format(r.status_code, "Gone"))
-    elif r.status_code == 411:
-        print("Status Code: {}({})".format(r.status_code, "Length Required"))
-    elif r.status_code == 412:
-        print("Status Code: {}({})".format(r.status_code, "Precondition Failed"))
-    elif r.status_code == 413:
-        print("Status Code: {}({})".format(r.status_code, "Payload Too Large"))
-    elif r.status_code == 414:
-        print("Status Code: {}({})".format(r.status_code, "URI Too Long"))
-    elif r.status_code == 415:
-        print("Status Code: {}({})".format(r.status_code, "Unsupported Media Type"))
-    elif r.status_code == 416:
-        print("Status Code: {}({})".format(r.status_code, "Range Not Satisfiable"))
-    elif r.status_code == 417:
-        print("Status Code: {}({})".format(r.status_code, "Expectation Failed"))
-    elif r.status_code == 418:
-        print("Status Code: {}({})".format(r.status_code, "I'm a teapot"))
-    elif r.status_code == 421:
-        print("Status Code: {}({})".format(r.status_code, "Misdirected Request"))
-    elif r.status_code == 422:
-        print("Status Code: {}({})".format(r.status_code, "Unprocessable Entity"))
-    elif r.status_code == 423:
-        print("Status Code: {}({})".format(r.status_code, "Locked"))
-    elif r.status_code == 424:
-        print("Status Code: {}({})".format(r.status_code, "Failed Dependency"))
-    elif r.status_code == 425:
-        print("Status Code: {}({})".format(r.status_code, "Too Early"))
-    elif r.status_code == 426:
-        print("Status Code: {}({})".format(r.status_code, "Upgrade Required"))
-    elif r.status_code == 428:
-        print("Status Code: {}({})".format(r.status_code, "Precondition Required"))
-    elif r.status_code == 429:
-        print("Status Code: {}({})".format(r.status_code, "Too Many Requests"))
-    elif r.status_code == 431:
-        print("Status Code: {}({})".format(r.status_code, "Request Header Fields Too Large"))
-    elif r.status_code == 451:
-        print("Status Code: {}({})".format(r.status_code, "Unavailable For Legal Reasons"))
-    elif r.status_code == 500:
-        print("Status Code: {}({})".format(r.status_code, "Internal Server Error"))
-    elif r.status_code == 501:
-        print("Status Code: {}({})".format(r.status_code, "Not Implemented"))
-    elif r.status_code == 502:
-        print("Status Code: {}({})".format(r.status_code, "Bad Gateway"))
-    elif r.status_code == 503:
-        print("Status Code: {}({})".format(r.status_code, "Service Unavailable"))
-    elif r.status_code == 504:
-        print("Status Code: {}({})".format(r.status_code, "Gateway Timeout"))
-    elif r.status_code == 505:
-        print("Status Code: {}({})".format(r.status_code, "HTTP Version Not Supported"))
-    elif r.status_code == 506:
-        print("Status Code: {}({})".format(r.status_code, "Variant Also Negotiates"))
-    elif r.status_code == 507:
-        print("Status Code: {}({})".format(r.status_code, "Insufficient Storage"))
-    elif r.status_code == 508:
-        print("Status Code: {}({})".format(r.status_code, "Loop Detected"))
-    elif r.status_code == 510:
-        print("Status Code: {}({})".format(r.status_code, "Not Extended"))
-    elif r.status_code == 511:
-        print("Status Code: {}({})".format(r.status_code, "Network Authentication Required"))
+    codes = [100, 101, 102, 103, 200, 201, 202, 203, 204, 205, 206, 207, 208, 226, 300, 301, 302, 303, 304, 305, 306,
+             307, 308, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418,
+             421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511]
+    messages = ["Continue", "Switching Protocol", "Processing", "Early Hints", "OK", "Created",
+                "Non-Authoritative Information", "Switching Protocol", "No Content", "Reset Content", "Partial Content"
+                , "Multi-Status", "Already Reported", "IM Used", "Multiple Choice", "Moved Permanently", "Found",
+                "See Other", "Not Modified", "Use Proxy", "Temporary Redirect", "Permanent Redirect", "Bad Request",
+                "Unauthorized", "Payment Required", "Forbidden", "Not Found", "Method Not Allowed", "Not Acceptable",
+                "Proxy Authentication Required", "Request Timeout", "Conflict", "Gone", "Length Required",
+                "Precondition Failed", "Payload Too Large", "URI Too Long", "Unsupported Media Type",
+                "Range Not Satisfiable", "Expectation Failed", "I'm a teapot", "Misdirected Request",
+                "Unprocessable Entity", "Locked", "Failed Dependency", "Too Early", "Upgrade Required",
+                "Precondition Required", "Too Many Requests", "Request Header Fields Too Large",
+                "Unavailable For Legal Reasons", "Internal Server Error", "Not Implemented", "Bad Gateway",
+                "Service Unavailable", "Gateway Timeout", "HTTP Version Not Supported", "Variant Also Negotiates",
+                "Insufficient Storage", "Loop Detected", "Not Extended", "Network Authentication Required"]
+    for i in range(0, len(codes)):
+        if codes[i] == r.status_code:   # We iterate through each status code using it's index then when it is equal
+            # to the request's status code, we print it along with the corresponding message
+            print(codes[i], messages[i])
 
 
 def request():  # Our main function that we can use with our URL
@@ -187,12 +83,12 @@ def history():  # This function shows the response objects that were created in 
     print(r.history)
 
 
-def header():   # This function shows us the header of the URL we requested
+def header():  # This function shows us the header of the URL we requested
     request()
     print(r.headers)
 
 
-def cookie():   # We can see if the website is using any cookies with us.
+def cookie():  # We can see if the website is using any cookies with us.
     request()
     print(r.cookies)
 
@@ -202,11 +98,11 @@ def content():  # We can see the contents of the website in human readable form
     print(r.text)
 
 
-def end_prog(): # Ends the program. Created to complete an if condition.
+def end_prog():  # Ends the program. Created to complete an if condition.
     exit()
 
 
-if "-u" in sys.argv:    # Makes sure the request() is called only when the appropriate switch is there
+if "-u" in sys.argv:  # Makes sure the request() is called only when the appropriate switch is there
     request()
     if "--headers" in sys.argv:
         header()
@@ -222,4 +118,3 @@ if "-u" in sys.argv:    # Makes sure the request() is called only when the appro
         history()
     else:
         end_prog()
-
